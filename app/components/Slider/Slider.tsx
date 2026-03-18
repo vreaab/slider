9e5cd7ecc8c0bec9551c2420cfa50de44840d303
Jonathan Yngfors <jonathanyngfors@MacBook-Pro-som-tillhor-Jonathan.local>
2026-03-18 08:23:55 +0100

    focus and hover

--- app/components/Slider/Slider.tsx
+++ app/components/Slider/Slider.tsx
@@ -1,38 +1,51 @@
 import { cn } from "@/lib/cn";
-import { Input } from "@headlessui/react";
+import { Field, Input, Label } from "@headlessui/react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-export default function Slider() {
+const sliderVariants = cva([
+  "mt-3 block w-full appearance-none rounded-lg border-none bg-transparent px-3 py-1.5 text-sm/6 text-white",
+  // Track styling (WebKit: Chrome, Safari, Edge)
+  "[&::-webkit-slider-runnable-track]:rounded-full",
+  "[&::-webkit-slider-runnable-track]:bg-[#55575e]",
+  "[&::-webkit-slider-runnable-track]:h-1",
+  // Track styling (Firefox)
+  "[&::-moz-range-track]:rounded-full",
+  "[&::-moz-range-track]:bg-[#55575e]",
+  "[&::-moz-range-track]:h-1",
+  // Thumb styling (WebKit: Chrome, Safari, Edge)
+  "[&::-webkit-slider-thumb]:appearance-none",
+  "[&::-webkit-slider-thumb]:rounded-full",
+  "[&::-webkit-slider-thumb]:cursor-pointer",
+  "[&::-webkit-slider-thumb]:h-5 [&::-webkit-slider-thumb]:w-5",
+  "[&::-webkit-slider-thumb]:-mt-2",
+  "[&::-webkit-slider-thumb]:bg-[#1d75cd]",
+  // Hover on thumb (WebKit)
+  "hover:[&::-webkit-slider-thumb]:bg-[#2586e0]",
+  // Focus ring on thumb (WebKit)
+  "focus:[&::-webkit-slider-thumb]:shadow-[0_0_0_2px_black]",
+  // Thumb styling (Firefox)
+  "[&::-moz-range-thumb]:rounded-full",
+  "[&::-moz-range-thumb]:border-none",
+  "[&::-moz-range-thumb]:cursor-pointer",
+  "[&::-moz-range-thumb]:h-5 [&::-moz-range-thumb]:w-5",
+  "[&::-moz-range-thumb]:bg-[#1d75cd]",
+  // Hover on thumb (Firefox)
+  "hover:[&::-moz-range-thumb]:bg-[#2586e0]",
+  // Focus ring on thumb (Firefox)
+  "focus:[&::-moz-range-thumb]:shadow-[0_0_0_2px_black]",
+  // Remove default focus outline
+  "focus:outline-none",
+]);
+
+type SliderProps = VariantProps<typeof sliderVariants> & {
+  className?: string;
+};
+
+export default function Slider({ className }: SliderProps) {
   return (
-    <div className="w-full max-w-md px-4">
-      <Input
-        type="range"
-        className={cn(
-          "mt-3 block w-full appearance-none rounded-lg border-none bg-transparent px-3 py-1.5 text-sm/6 text-white",
-          // Track styling (WebKit: Chrome, Safari, Edge)
-          "[&::-webkit-slider-runnable-track]:h-1",
-          "[&::-webkit-slider-runnable-track]:rounded-full",
-          "[&::-webkit-slider-runnable-track]:bg-[#55575e]",
-          // Track styling (Firefox)
-          "[&::-moz-range-track]:h-1",
-          "[&::-moz-range-track]:rounded-full",
-          "[&::-moz-range-track]:bg-[#55575e]",
-          // Thumb styling (WebKit: Chrome, Safari, Edge)
-          "[&::-webkit-slider-thumb]:appearance-none",
-          "[&::-webkit-slider-thumb]:h-5 [&::-webkit-slider-thumb]:w-5",
-          "[&::-webkit-slider-thumb]:rounded-full",
-          "[&::-webkit-slider-thumb]:bg-[#1d75cd]",
-          "[&::-webkit-slider-thumb]:cursor-pointer",
-          "[&::-webkit-slider-thumb]:-mt-2",
-          // Thumb styling (Firefox)
-          "[&::-moz-range-thumb]:h-5 [&::-moz-range-thumb]:w-5",
-          "[&::-moz-range-thumb]:rounded-full",
-          "[&::-moz-range-thumb]:border-none",
-          "[&::-moz-range-thumb]:bg-[#1d75cd]",
-          "[&::-moz-range-thumb]:cursor-pointer",
-          // Focus
-          "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-black",
-        )}
-      />
-    </div>
+    <Field className="w-full max-w-md px-4">
+      <Label className="sr-only">Slider</Label>
+      <Input type="range" className={cn(sliderVariants(), className)} />
+    </Field>
   );
 }
